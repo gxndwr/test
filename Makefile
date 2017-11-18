a8f26c4892f39e79b20113d7a05ea29981e73f46
Ran Wang <gxndwr@outlook.com>
2017-11-18 15:57:32 +0800

    Move number generating to number.c
    Signed-off-by: Ran Wang <gxndwr@outlook.com>

--- Makefile
+++ Makefile
@@ -1,2 +1,2 @@
-test: main.c io.h io.c
-	gcc main.c io.c -o $@
+test: main.c io.h io.c number.h number.h
+	gcc main.c io.c number.c -o $@
--- main.c
+++ main.c
@@ -5,9 +5,10 @@
 #include <sys/timeb.h>
 #include <unistd.h>
 #include "io.h"
+#include "number.h"
 
 #define QUESTION_NUM    8
-#define RADOM_SEED_POOL_SIZE 10
+
 
 #if 0
 #define DEBUG
@@ -22,56 +23,6 @@ void dbg(char *format, ...)
 }
 #endif
 
-struct random_seed {
-    struct random_seed *next;
-    struct random_seed *prev;
-    int seed;
-};
-static struct random_seed *head_seed;
-static struct random_seed *enqueue_seed;
-static struct random_seed *dequeue_seed;
-static struct random_seed first_seed;
-void print_random_seed_content(void)
-{
-    struct random_seed *p;
-    printf("\nprint all linked list items:\n");
-    p = head_seed;
-    while(p->next != head_seed) {
-        printf("p->prev: %p\n", p->prev);
-        printf("p      : %p\n", p);
-        printf("p->seed      : %d\n", p->seed);
-        printf("p->next: %p\n", p->next);
-        p = p->next;
-        printf("-------------\n");
-    }
-    printf("p->prev: %p\n", p->prev);
-    printf("p      : %p\n", p);
-    printf("p->seed      : %d\n", p->seed);
-    printf("p->next: %p\n", p->next);
-}
-void initialize_random_seed_poll(void)
-{
-    int i, ret;
-    struct random_seed *p;
-
-    head_seed = &first_seed;
-    enqueue_seed = head_seed;
-    dequeue_seed = head_seed;
-    head_seed->next = &first_seed;
-    head_seed->prev = &first_seed;
-
-    dbg("struct size:%lu\n", sizeof(struct random_seed));
-    for (i=0; i<RADOM_SEED_POOL_SIZE; i++) {
-        p = malloc(sizeof(struct random_seed));
-        p->seed = i*i; /* make default digit hard to predict */
-        p->next = head_seed;
-        p->prev = head_seed->prev;
-        head_seed->prev->next = p;
-        head_seed->prev = p;
-        head_seed = p;
-    }
-}
-
 static struct timeb timeSeed;
 char get_input()
 {
@@ -79,18 +30,6 @@ char get_input()
     enqueue_seed->seed = timeSeed.time * 1000 + timeSeed.millitm;
     enqueue_seed = enqueue_seed->next;
     return getchar();
-}
-
-int get_random_digits(int mod)
-{
-    int digit;
-
-	srand(dequeue_seed->prev->seed);
-    dequeue_seed = dequeue_seed->next;
-	digit = rand()%mod;
-    dbg("\nrandom seed:%d\n", dequeue_seed->seed);
-    dbg("\nrandom digit: %d\n", digit);
-    return digit;
 }
 
 enum operation {ADD, SUB, MUX, DIV, ERR};
